dbd4952e766948cdd95377a53701262f0e735f0f
Darío Marcos <37598162+ZenithGD@users.noreply.github.com>
2025-01-01 16:35:53 +0100

    push tables

---
+++ src/db/tables.ts
@@ -0,0 +1,28 @@
+import { blob, integer, primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const puzzle = sqliteTable("puzzle", {
+  id: integer('id').primaryKey(),
+  es_title: text('title').notNull(),
+  en_title: text('title').notNull(),
+  fr_title: text('title').notNull(),
+  description: text('description'),
+  size: integer('size').notNull().default(3),
+  difficulty: integer('difficulty').notNull().default(1),
+  image: text('image').notNull()
+});
+
+export type PuzzleRowType = typeof puzzle.$inferSelect;
+export type PuzzleRowInsertType = typeof puzzle.$inferInsert;
+
+export const ranking = sqliteTable("ranking", {
+  id: integer('id').references(() => puzzle.id).notNull(),
+  uname: text('uname').notNull(),
+  solveTS: integer('solveTS')
+  .notNull()
+}, ranking => ({
+    pk: primaryKey({ columns: [ranking.id, ranking.uname] })
+  })
+)
+
+export type RankingRowType = typeof ranking.$inferSelect;
+export type RankingRowInsertType = typeof ranking.$inferInsert;
